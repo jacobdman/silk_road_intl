c4cb4e533a21fdca6a2390e88daf84cc534688c2
Jacob <jacobdman5@gmail.com>
2018-11-07 10:31:58 -0700

    set up

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './Components/Home/Home';
+
+export default (
+    <Switch>
+        <Route component={Home} exact path='/'/>
+    </Switch>
+)
